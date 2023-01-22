3a2235a008b424993404d07483940ef6f0628603
Patryk Pozniak <ppozniak95@gmail.com>
2023-01-22 20:57:45 +0000

    Create Todo item

---
+++ src/components/TodoItem.vue
@@ -0,0 +1,35 @@
+<script setup lang="ts">
+import type { ITodo } from 'types/todo';
+
+const { todo } = defineProps<{
+  todo: ITodo,
+}>()
+
+const { id, body, username } = todo;
+
+</script>  
+
+<template>
+  <li class="todo">
+    <div>
+      {{ body }}
+    </div>
+    <footer>
+      <address rel="author">
+        {{ username }}
+      </address>
+    </footer>
+    <!-- @TODO: Show delete button only for author -->
+    <button type="button" @click="$emit('onDeleteClick', id)">
+      Delete that
+    </button>
+  </li>
+</template>
+
+<style scoped>
+.todo {
+  padding: 0.3rem;
+  background-color: yellow;
+  list-style-type: none;
+}
+</style>
--- src/pages/BoardPage.vue
+++ src/pages/BoardPage.vue
@@ -3,6 +3,7 @@ import router from '@/router';
 import { socket } from '@/services/socket';
 import { onUnmounted, onMounted, ref } from 'vue';
 import type { ITodo } from '@/../types/todo';
+import TodoItem from '@/components/TodoItem.vue';
 
 if (!socket.connected) {
   // @TODO: This will be replaced once auth is persisted
@@ -39,24 +40,11 @@ function handleDelete(id: ITodo["id"]) {
 
 <template>
   <ul class="todos">
-    <li v-for="{ body, username, id } in todos" class="todo">
-      <div>
-        {{ body }}
-      </div>
-      <footer>
-        <address rel="author">
-          {{ username }}
-        </address>
-      </footer>
-      <!-- @TODO: Show only for author -->
-      <button type="button" @click="handleDelete(id)">
-        Delete that
-      </button>
-    </li>
+    <TodoItem v-for="todo in todos" :todo="todo" :key="todo.id" @onDeleteClick="handleDelete" />
   </ul>
-<button @click="handleCreate" type="button">
-  Create new todo
-</button>
+  <button @click="handleCreate" type="button">
+    Create new todo
+  </button>
 </template>
 
 <style scoped>
@@ -66,11 +54,5 @@ function handleDelete(id: ITodo["id"]) {
   grid-gap: 1rem;
   grid-template-columns: repeat(auto-fill, minmax(200px, 1fr));
   grid-auto-rows: minmax(150px, auto);
-}
-
-.todo {
-  padding: 0.3rem;
-  background-color: yellow;
-  list-style-type: none;
 }
 </style>
