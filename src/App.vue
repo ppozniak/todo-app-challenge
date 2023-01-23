22abe19093de3f5bb9b33915cbbdac790203956a
Patryk Pozniak <ppozniak95@gmail.com>
2023-01-23 01:31:17 +0000

    Style login page and app

--- src/App.vue
+++ src/App.vue
@@ -6,9 +6,18 @@ import "./index.css";
 
 onUnmounted(() => {
   socket.disconnect();
-})
+});
 </script>
 
 <template>
-  <RouterView />
+  <div class="bg-slate-700 h-full text-white">
+    <header class="py-2 mb-2 bg-emerald-700 bg-opacity-80 shadow-lg">
+      <div class="container">
+        <h1>TODO.io</h1>
+      </div>
+    </header>
+    <main class="container">
+      <RouterView />
+    </main>
+  </div>
 </template>
--- src/pages/LoginPage.vue
+++ src/pages/LoginPage.vue
@@ -1,66 +1,75 @@
 <script setup lang="ts">
 import { ref, onUnmounted } from "vue";
-import { socket } from '@/services/socket'
-import router from "@/router";
+import { socket } from "@/services/socket";
 import { useUserStore } from "@/stores/user";
+import { useRouter } from "vue-router";
 
 // @TODO: Add persistance to user and redirect to board if logged in
-const userStore = useUserStore()
+const userStore = useUserStore();
+const router = useRouter();
 
 let username = ref("");
-let loading = ref(false)
-let errorMessage = ref('')
+let loading = ref(false);
+let errorMessage = ref("");
 
 function handleSubmit() {
   loading.value = true;
-  errorMessage.value = '';
+  errorMessage.value = "";
 
-  socket.auth = { username: username.value }
+  socket.auth = { username: username.value };
   socket.connect();
 }
 
+// @TODO: Split this into a hook?
 socket.on("connect_error", (error) => {
   console.log(error);
   errorMessage.value = error.message;
   loading.value = false;
 });
 
-socket.on('connect', () => {
+socket.on("connect", () => {
+  console.log("Should not fire... sus");
   loading.value = false;
   userStore.login(username.value);
-  router.replace('/board')
-})
+  router.replace("/board");
+});
 
 onUnmounted(() => {
-  socket.off('connect_error');
-  socket.off('connect');
-})
+  socket.off("connect_error");
+  socket.off("connect");
+});
 </script>
 
 <template>
-  <form @submit.prevent="handleSubmit">
-    <label class="label" for="username">Username</label>
-    <input type="text" v-model="username" id="username" placeholder="Enter your username..." />
-    <button type="submit">Login</button>
+  <div class="mx-auto mt-8 md:mt-[10vh] max-w-xl">
+    <form @submit.prevent="handleSubmit" class="flex flex-col gap-2">
+      <div class="flex flex-col">
+        <label class="font-bold tracking-wide" for="username">Username</label>
+        <input
+          type="text"
+          v-model="username"
+          id="username"
+          placeholder="Enter your username..."
+          class="p-2 rounded-lg text-black"
+        />
+      </div>
+
+      <button
+        type="submit"
+        class="py-1 px-3 rounded-full border-transparent bg-emerald-600 font-bold hover:bg-emerald-700 transition-colors shadow-sm"
+        :disabled="loading"
+      >
+        Login
+      </button>
     </form>
-    <div v-if="loading">
-      Joining... please wait.
+
+    <strong
+      class="px-4 py-1 rounded-sm mt-4 inline-block"
+      :class="{ 'bg-red-700': !!errorMessage }"
+      :aria-live="errorMessage ? 'polite' : 'off'"
+      v-if="errorMessage || loading"
+    >
+      {{ errorMessage || "Joining... please wait." }}
+    </strong>
   </div>
-  <strong class="error" v-if="errorMessage">
-    {{ errorMessage }}
-  </strong>
 </template>
-
-
-<style scoped>
-.label {
-  display: block;
-  font-weight: bold;
-  margin-bottom: 3px;
-}
-
-.error {
-  font-weight: bold;
-  color: red;
-}
-</style>
